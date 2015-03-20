b42eceb7e2b782fcb78de0d300b732acd0f5dca7
olivier <oliserand@gmail.com>
2015-03-20 16:32:12 +0200

    Q5.py

--- Q5.py
+++ Q5.py
@@ -5,4 +5,18 @@ create a program that ask the user for the number of terms used to calculate the
 value
 '''
 
+running = True
+while running:
+    e = 1
+    n = input('Enter number of terms used to approximate e: ')
+    if n == '':
+        running = False
+    else:
+        n = int(n)
+        for i in range(1,n+1):
+            factorial = 1
+            for j in range(1, i+1):
+                factorial *= j
+            e += (1/factorial)
 
+        print(e)
